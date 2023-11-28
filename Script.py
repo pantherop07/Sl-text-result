aad9241649598bae5c3ef2918f8532b7343273ad
pantherop07 <139424054+pantherop07@users.noreply.github.com>
2023-11-28 22:04:20 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -88,7 +88,7 @@ I can't find the <b>{}</b> in my database! 🥲
 🗣 Requested by: {message.from_user.mention}
 ©️ Powered by: <b>{message.chat.title}</b>"""
 
-    FILE_CAPTION = """<i>{file_name}</i>
+    FILE_CAPTION = """<b>[{file_name}](https://t.me/+guasifWW0BgzMmJl)</b>
 
 🚫 ᴘʟᴇᴀsᴇ ᴄʟɪᴄᴋ ᴏɴ ᴛʜᴇ ᴄʟᴏsᴇ ʙᴜᴛᴛᴏɴ ɪꜰ ʏᴏᴜ ʜᴀᴠᴇ sᴇᴇɴ ᴛʜᴇ ᴍᴏᴠɪᴇ 🚫"""
 
