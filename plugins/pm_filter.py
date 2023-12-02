0170cff1b42b6108320a06a42a21fccbe56aad18
pantherop07 <139424054+pantherop07@users.noreply.github.com>
2023-12-02 12:02:32 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -12,7 +12,7 @@ from info import ADMINS, URL, BIN_CHANNEL, DELETE_TIME, AUTH_CHANNEL, IS_VERIFY,
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from pyrogram import Client, filters, enums
 from pyrogram.errors import FloodWait, UserIsBlocked, MessageNotModified, PeerIdInvalid, ChatAdminRequired
-from utils import get_size, is_subscribed, is_check_admin, get_wish, get_shortlink, get_verify_status, update_verify_status, get_readable_time, get_poster, temp, get_settings, save_group_settings
+from utils import get_size, is_subscribed, is_check_admin, get_wish, get_shortlink, get_verify_status, update_verify_status, get_readable_time, get_poster, temp, get_settings, save_group_settings, get_hash
 from database.users_chats_db import db
 from database.ia_filterdb import Media, get_file_details, get_search_results,delete_files
 import logging
@@ -29,8 +29,14 @@ async def stream_downloader(bot, query):
     msg = await bot.send_cached_media(
         chat_id=BIN_CHANNEL,
         file_id=file_id)
-    online = f"{URL}watch/{msg.id}"
-    download = f"{URL}download/{msg.id}"
+    online = f"https://{URL}/watch/{msg.id}?hash={get_hash(msg)}"
+    download = f"https://{URL}/{msg.id}?hash={get_hash(msg)}"
+    btn= [[
+        InlineKeyboardButton("ᴡᴀᴛᴄʜ ᴏɴʟɪɴᴇ", url=online),
+        InlineKeyboardButton("ꜰᴀsᴛ ᴅᴏᴡɴʟᴏᴀᴅ", url=download)
+    ],[
+        InlineKeyboardButton('❌ ᴄʟᴏsᴇ ❌', callback_data='close_data')
+    ]]
     await query.edit_message_reply_markup(
         reply_markup=InlineKeyboardMarkup(
         [
